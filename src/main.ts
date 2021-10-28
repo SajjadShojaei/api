89ad37f7078591dab10248b892974a15c546a3f5
Sajjad Shojaei Moghaddam <86704788+SajjadShojaei@users.noreply.github.com>
2021-10-28 15:15:05 +0330

    api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  const config=new DocumentBuilder()
+  .setTitle('linkedin')
+  .setDescription('linkedin swagger')
+  .setVersion('1.0')
+  .addTag('linkedin')
+  .build()
+  const document=SwaggerModule.createDocument(app,config)
+  SwaggerModule.setup('docs',app,document)
   await app.listen(3000);
 }
 bootstrap();
